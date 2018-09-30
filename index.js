05b093b7b91c711904ee2391d329ba19886f063b
Eddy Wu <java.gdb@gmail.com>
2018-09-30 14:42:03 +1000

    add first method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let request = require('request');
 
 class nodelib {
 
@@ -5,10 +6,32 @@ class nodelib {
         
     }
 
-    async function test() {
+    async test() {
         return "test information!!!";
+    }
+
+    async require(url, method, data) {
+      return new Promise((resolve, reject) => {
+        request({
+          url,
+          method,
+          json: data
+        }, (err, resp, body) => {
+          if (err) return reject(err);
+
+          const statusCode = resp.statusCode;
+
+          if (statusCode !== 200) {
+            let msg = body.message || 'Unknown error from VPE';
+
+            return reject(new Error(msg));
+          }
+          return resolve(body);
+        });
+      });
+        
     }
     
 }
 
-module.exports = nodelib;
+module.exports = new nodelib();
---
+++ test.js
@@ -0,0 +1,17 @@
+
+
+let nodelibClass = require('./index');
+//let nodelibTest = new nodelibClass();
+let nodelibTest = nodelibClass;
+
+const url = "https://jsonplaceholder.typicode.com/posts/2";
+const method = 'get';
+const data = {'id': '2'};
+
+async function test() {
+    console.log(await nodelibTest.require(url,method,data));
+    process.exit();
+}
+
+test();
+
