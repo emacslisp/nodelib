a7f848d4290f6820d0c818ac105f545fdbda7894
Eddy Wu <java.gdb@gmail.com>
2018-10-10 16:11:16 +1100

    add random string

--- test/testCryptoHelper.js
+++ test/testCryptoHelper.js
@@ -3,7 +3,9 @@ let CryptoHelperClass = require('../lib/CryptoHelper');
 let cryptoHelper = new CryptoHelperClass();
 
 console.log(cryptoHelper.md5str('test'));
-console.log(cryptoHelper.generateHash('test','123'));
+console.log("generateHash test: " + cryptoHelper.generateHash('test','123'));
 
 
-console.log(cryptoHelper.generateSalt());
+console.log("generateSalt salt: " + cryptoHelper.generateSalt());
+
+console.log("randomString test: " + cryptoHelper.randomString(12));
