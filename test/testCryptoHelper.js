064cca4a28e76fdc27fb3aca0c31a8a93c60856a
Eddy Wu <java.gdb@gmail.com>
2018-10-10 16:07:47 +1100

    add generate Salt

--- test/testCryptoHelper.js
+++ test/testCryptoHelper.js
@@ -4,3 +4,6 @@ let cryptoHelper = new CryptoHelperClass();
 
 console.log(cryptoHelper.md5str('test'));
 console.log(cryptoHelper.generateHash('test','123'));
+
+
+console.log(cryptoHelper.generateSalt());
