d3bcff114e08cea9403842844dfa9bb7e5c1b2d3
Eddy Wu <java.gdb@gmail.com>
2018-10-12 14:19:55 +1100

    finish log4js config

--- test/testLog4jsHelper.js
+++ test/testLog4jsHelper.js
@@ -5,3 +5,6 @@ let Log4jsHelper = new Log4jsClass();
 Log4jsHelper.trace('this is a test');
 Log4jsHelper.debug('this is a test');
 Log4jsHelper.info('this is a test');
+Log4jsHelper.warn('this is a test');
+Log4jsHelper.error('this is a test');
+Log4jsHelper.fatal('this is a test');
