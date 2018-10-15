ba2940cc9d47f6412b27fa859f58c4d50ed12c0e
Eddy Wu <java.gdb@gmail.com>
2018-10-15 11:19:14 +1100

    finish auth

--- test/testAuth.js
+++ test/testAuth.js
@@ -1,6 +1,9 @@
 let nodelibClass = require('../index');
 //let nodelibTest = new nodelibClass();
-let nodelibTest = nodelibClass;
+let nodelibTest = new nodelibClass.nodelib();
+
+let authClass = require('../lib/AuthHelper');
+let authHelper = new authClass();
 
 const url = 'https://vocuscommunicationstest.service-now.com/api/vomml/v2/vone/list/change?am=nicholas.cheah@vocus.com.au';
 const method = 'GET';
@@ -11,6 +14,9 @@ const password = 'U^tw9ks6wyM2@sq';
 
 async function test() {
     console.log(nodelibTest.jsonFormat(await nodelibTest.requestWithBasicAuth(url, method, data, header, username, password)));
+    // a7f5c8c626f994482813230854f66700e626208f52d913b9bd6b4e039aab0f41
+    let passwordMatch = authHelper.verify('test', '123', 'a7f5c8c626f994482813230854f66700e626208f52d913b9bd6b4e039aab0f41');
+    console.log("passwordMatch : " + passwordMatch);
     process.exit();
 }
 
