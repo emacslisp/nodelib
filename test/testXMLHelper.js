e1e212fc23c61b4784aaa1ef5e8c997d5ba1d2f9
Eddy Wu <java.gdb@gmail.com>
2018-10-18 16:25:46 +1100

    finish xml2js encapsulation

---
+++ test/testXMLHelper.js
@@ -0,0 +1,21 @@
+let xmlClass = require('../lib/XMLHelper');
+
+let xmlHelper = new xmlClass();
+
+async function test() {
+
+    let xml = "<root>Hello xml2js!</root>";
+    let result = await xmlHelper.parseXML(xml);
+    console.log(result);
+    process.exit(0);
+}
+
+async function buildXMLTest() {
+    var obj = {name: "Super", Surname: "Man", age: 23};
+    let result = xmlHelper.buildXML(obj);
+    console.log(result);
+    process.exit(0);
+}
+
+test();
+//buildXMLTest();
