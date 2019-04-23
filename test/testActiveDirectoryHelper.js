b2356cffb0a735ca6421c8b566b66a8a5b0fa128
Eddy Wu <java.gdb@gmail.com>
2019-04-23 17:16:25 +1000

    #ActiveDirectoryHelper

---
+++ test/testActiveDirectoryHelper.js
@@ -0,0 +1,12 @@
+let ActiveDirectoryClass = require('../lib/ActiveDirectoryHelper');
+
+let activeDirectory = new ActiveDirectoryClass();
+
+
+async function ActiveDirectoryTest() {
+    let username = Buffer.from('ZGkud3UudmV4QHZvY3VzLmV4dA==', 'base64').toString();;
+    let password = Buffer.from('cGk9My4xNDE1', 'base64').toString();
+    activeDirectory.ADAuth(username, password);
+}
+
+ActiveDirectoryTest();
