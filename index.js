8372bb42ca8f8bfb90e2a54587c971989bb53e0b
Eddy Wu <java.gdb@gmail.com>
2019-08-28 16:04:40 +1000

    export XMLHelper

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ let FileHelper = require('./lib/FileHelper');
 let RequestHelper = require('./lib/RequestHelper');
 let Log4jsHelper = require('./lib/Log4jsHelper');
 let RedisHelper = require('./lib/RedisHelper');
+let XMLHelper = require('./lib/XMLHelper');
 
 module.exports = {
   FileHelper,
   RequestHelper,
   Log4jsHelper,
-  RedisHelper
+  RedisHelper,
+  XMLHelper
 }
