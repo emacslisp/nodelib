5cdf9b025a826df557cb3503fbbaf724c33868a3
Eddy Wu <java.gdb@gmail.com>
2019-11-11 11:24:32 +1100

    expose jwtservice

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ let RequestHelper = require('./lib/RequestHelper');
 let Log4jsHelper = require('./lib/Log4jsHelper');
 let RedisHelper = require('./lib/RedisHelper');
 let XMLHelper = require('./lib/XMLHelper');
+let JWTService = require('./lib/JWTService');
 
 module.exports = {
   FileHelper,
   RequestHelper,
   Log4jsHelper,
   RedisHelper,
-  XMLHelper
+  XMLHelper,
+  JWTService
 }
